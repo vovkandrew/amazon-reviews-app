f70c9b0c94b433eee041f087873479b3e6d7ad76
VovkAA <vovkandrew@gmail.com>
2020-07-18 18:08:04 +0300

    Done

--- src/main/java/amazonreviewsapp/springboot/controller/AuthController.java
+++ src/main/java/amazonreviewsapp/springboot/controller/AuthController.java
@@ -12,7 +12,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
