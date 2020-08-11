e488ffe9d6db847a6d7647b893f07286282b0b86
VovkAA <vovkandrew@gmail.com>
2020-08-11 20:55:07 +0300

    Parse controller fixed, path passing simplified and improved

--- src/main/java/amazonreviewsapp/springboot/controller/ParseController.java
+++ src/main/java/amazonreviewsapp/springboot/controller/ParseController.java
@@ -1,21 +1,34 @@
 package amazonreviewsapp.springboot.controller;
 
 import amazonreviewsapp.springboot.service.CSVFileParserService;
-import java.io.File;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class ParseController {
     @Autowired
     private CSVFileParserService parser;
 
+    @Value("${win.filepath}")
+    private String PATH_WIND;
+
+    @Value("${lin.filepath}")
+    private String PATH_LIN;
+
     @GetMapping
     @RequestMapping("/parse")
     public String startParsing() {
-        parser.parseCsvFile("/tmp/Reviews.csv");
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            parser.parseCsvFile(PATH_WIND);
+
+        } if (System.getProperty("os.name").startsWith("Linux")) {
+            parser.parseCsvFile(PATH_LIN);
+        }
         return "Parsing has been initialized";
     }
 }
